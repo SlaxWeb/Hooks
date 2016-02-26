39ec9445e51fc3f022fcbcca1f0f9591be1bde40
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-02-26 17:54:03 +0100

    add the container class and its initialization

---
+++ src/Container.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Hooks Container Class
+ *
+ * The Container class holds all user definitions for hook points in the form of
+ * Hook class objects. It also provides methods for adding new definitions, and
+ * execution of those user definitions.
+ *
+ * @package   SlaxWeb\Config
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.4
+ */
+namespace SlaxWeb\Hooks;
+
+class Container
+{
+    /**
+     * Logger
+     *
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $_logger = null;
+
+    /**
+     * Class constructor
+     *
+     * Instantiates the Container, and sets the Logger to the protected proprty
+     * and writes the successful init message to the logger.
+     *
+     * @param \Psr\Log\LoggerInterface $logger Logger that implements the PSR
+     *                                         interface
+     */
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->_logger = $logger;
+
+        $this->_logger->addInfo("Hooks component initialized");
+    }
+}
